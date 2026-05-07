bf42e78b2071995a743a2ca7746cf99d327f14a4
Joi McBrayer <j0ifulnois3@gmail.com>
2026-05-07 13:50:28 -0500

    Add App

---
+++ src/main/java/com/pluralsight/blackjack/App.java
@@ -0,0 +1,18 @@
+package com.pluralsight.blackjack;
+
+public class App {
+
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        Hand hand1 = new Hand();
+        // deal 5 cards
+        for(int i = 0; i < 5; i++) {
+            // get a card from the deck
+            Card card = deck.deal();
+            // deal that card to the hand
+            hand1.deal(card);
+        }
+        int handValue = hand1.getValue();
+        System.out.println("This hand is worth " + handValue);
+    }
+}
