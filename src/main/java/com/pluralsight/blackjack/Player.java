6d5972906a6372c88f5b207b7896b61abcfbc7aa
Joi McBrayer <j0ifulnois3@gmail.com>
2026-05-07 14:26:29 -0500

    Add Player

---
+++ src/main/java/com/pluralsight/blackjack/Player.java
@@ -0,0 +1,15 @@
+package com.pluralsight.blackjack;
+
+import com.sun.tools.javac.Main;
+
+import java.util.Scanner;
+
+public class Player {
+    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter All Player Names In Order: (Ex. John, Jacob, Jiggleheimer, Schmidt" );
+
+    }
+
+
+}
